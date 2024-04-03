724dda283beb77b1b8a20258c0007fa91bd6ff33
Trinh-Tan-Dat <21520714@gm.uit.edu.vn>
2024-04-03 20:53:07 +0700

    fix: config mongo db

--- database/config.js
+++ database/config.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`mongodb+srv://DatUIT:Dat@@123456789@cluster0.1trqkzp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,);
+    await mongoose.connect(`mongodb+srv://DatUIT:Dat123456@cluster0.1trqkzp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,);
     console.log("MongoDB connected");
   } catch (err) {
     console.error(err.message);
