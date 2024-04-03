37dfcf344cfc14e79e56fd25091a96813d2ff5c8
cmwuaan <minhquanrbpk@gmail.com>
2024-04-04 04:36:53 +0700

    feat: upload music

--- route/MusicRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { uploadMusic } = require("../controller/MusicController");
-
-router.route("/upload").post(uploadMusic);
-
-module.exports = router;
