5082dae1f9b5c0fd0fbe5175653bf6b6673342a5
Trinh-Tan-Dat <21520714@gm.uit.edu.vn>
2024-04-12 10:01:55 +0700

    feat: upload music file .mp3 to firebase

--- routes/MusicsRoutes.js
+++ routes/MusicsRoutes.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
-
+const multer = require('multer'); // Import multer
 const { uploadMusic, getMusics } = require('../controllers/MusicController');
 
+// Cấu hình multer để xử lý tải lên file nhạc
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/') // Thư mục lưu trữ file nhạc
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname) // Giữ nguyên tên file
+  }
+});
+
+const upload = multer({ storage: storage });
+
+// Route để lấy danh sách nhạc
 router.get('/', getMusics);
-router.post('/upload', uploadMusic);
+
+// Route để tải lên file nhạc
+router.post('/upload', upload.single('musicFile'), uploadMusic);
 
 module.exports = router;
