125a35e6a96c663c2b50d64d3d54c29ee585a767
Trinh-Tan-Dat <21520714@gm.uit.edu.vn>
2024-05-16 09:05:53 +0700

    feat: Add Firebase storage integration for uploading music files

--- controllers/MusicController.js
+++ controllers/MusicController.js
@@ -1,40 +1,52 @@
 const Music = require('../models/MusicModel');
+const { uploadBytes, getDownloadURL } = require('firebase/storage');
+const { firebaseStorage, ref } = require('../utils/Firebase');
 
 const uploadMusic = async (req, res) => {
   try {
-    const { musicName, genre, author, view, description, lyrics, url, imageUrl, releaseYear } = req.body;
+    const { musicName, genre, author, view, description, lyrics, releaseYear } = req.body;
 
     if (!musicName || !genre || !author || !description || !releaseYear) {
       return res.status(400).json({ message: 'Please fill in all required fields' });
     }
 
-    const music = new Music({
-      musicName: musicName,
-      genre: genre,
-      author: author,
-      view: view,
-      description: description,
-      lyrics: lyrics,
-      url: url,
-      imageUrl: imageUrl,
-      releaseYear: releaseYear,
-    });
-
-    try {
-      await music.save();
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
+    // Lấy file nhạc từ yêu cầu
+    const musicFile = req.file;
+    if (!musicFile) {
+      return res.status(400).json({ message: 'Please upload a music file' });
     }
 
-    res.status(201).json({ music: music });
+    // Tải file nhạc lên Firebase Storage
+    const storageRef = ref(firebaseStorage, `music/${musicFile.originalname}`);
+    const snapshot = await uploadBytes(storageRef, musicFile.buffer);
+
+    // Lấy URL của file nhạc đã tải lên
+    const url = await getDownloadURL(snapshot.ref);
+
+    // Tạo một đối tượng Music với thông tin từ req.body và URL của file nhạc
+    const music = new Music({
+      musicName,
+      genre,
+      author,
+      view,
+      description,
+      lyrics,
+      url,
+      releaseYear,
+    });
+
+    // Lưu thông tin nhạc vào MongoDB
+    await music.save();
+
+    res.status(201).json({ music });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
   }
 };
 
-const getMusicList = async (req, res) => {
+
+const getMusics = async (req, res) => {
   try {
     const musics = await Music.find();
     res.json(musics);
@@ -44,4 +56,4 @@ const getMusicList = async (req, res) => {
   }
 };
 
-module.exports = { uploadMusic, getMusicList };
+module.exports = { uploadMusic, getMusics };
